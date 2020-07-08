3cbccd91e8df8b9455c4bb6eec6568f1560438e8
Harpreet Aulakh <hkaulakh14@gmail.com>
2020-07-08 09:55:05 -0500

    Updates

---
+++ precache-manifest.4d3c8ed22662e675b5bdf1d7e76dae5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa502ac6654c91f32104b0f0d3d7eaf",
+    "url": "/%7BUser-Directory%7D/index.html"
+  },
+  {
+    "revision": "24a683f67ef1e26aade4",
+    "url": "/%7BUser-Directory%7D/static/css/main.9b9f736c.chunk.css"
+  },
+  {
+    "revision": "c297b18578962ffbd46d",
+    "url": "/%7BUser-Directory%7D/static/js/2.197463c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BUser-Directory%7D/static/js/2.197463c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a683f67ef1e26aade4",
+    "url": "/%7BUser-Directory%7D/static/js/main.7a8de423.chunk.js"
+  },
+  {
+    "revision": "5a49974403820f765a47",
+    "url": "/%7BUser-Directory%7D/static/js/runtime-main.0a29612c.js"
+  }
+]);
